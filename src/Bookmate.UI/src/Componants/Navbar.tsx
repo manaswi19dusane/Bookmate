5d85e57c56cc1ec9ff57d582873c5e64a0948f20
Manasvi dusane <manasvidusane2@gmail.com>
2026-01-05 06:28:49 +0530

    ui responsive

--- src/Bookmate.UI/src/Componants/Navbar.tsx
+++ src/Bookmate.UI/src/Componants/Navbar.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="nav">
       <h1 className="logo">Bookmate</h1>
 
-      <div className="nav-links">
-        <Link to="/">Books</Link>
-        <Link to="/add">Add Book</Link>
-        <Link to="/wishlist">Wishlist</Link>
+      {/* Hamburger icon (mobile) */}
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+
+      {/* Links */}
+      <div className={`nav-links ${open ? "show" : ""}`}>
+        <Link to="/" onClick={() => setOpen(false)}>Books</Link>
+        <Link to="/add" onClick={() => setOpen(false)}>Add Book</Link>
+        <Link to="/wishlist" onClick={() => setOpen(false)}>Wishlist</Link>
       </div>
     </nav>
   );
